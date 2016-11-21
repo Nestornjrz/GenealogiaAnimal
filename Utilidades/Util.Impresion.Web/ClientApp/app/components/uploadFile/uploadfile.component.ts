faa02474b27329a03207a6ee3c71c54e2e91b1c9
Nestornjrz <nestornjrz@hotmail.com>
2016-11-21 17:08:17 -0300

    Comentario qu aclara de donde saque la informacion para poder inicializar los eventos del componente FileUploader

--- Utilidades/Util.Impresion.Web/ClientApp/app/components/uploadFile/uploadfile.component.ts
+++ Utilidades/Util.Impresion.Web/ClientApp/app/components/uploadFile/uploadfile.component.ts
@@ -1,5 +1,5 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { FileSelectDirective ,  FileUploader } from 'ng2-file-upload';
+import { FileSelectDirective, FileUploader } from 'ng2-file-upload';//http://valor-software.com/ng2-file-upload/
 
 const URL = 'http://localhost:15125/api/uploadFile';
 
@@ -7,9 +7,12 @@ const URL = 'http://localhost:15125/api/uploadFile';
     selector: 'upload-file',
     template: require('./uploadfile.component.html')
 })
+
 export class UploadFileComponent implements OnInit {
     public uploader: FileUploader = new FileUploader({ url: URL });
 
+    /**  Gracias ha este enlace https://github.com/valor-software/ng2-file-upload/issues/251 
+      pude saber como registrar funciones a los eventos de FileUploader */
     ngOnInit() {
         this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
             console.log("Archivo subido" + response);            
