94d6588926733e2bd838015256871e483069e32a
Nestornjrz <nestornjrz@hotmail.com>
2016-11-21 11:21:41 -0300

    Se instalo los paquetes necesarios para usar LocalDb

---
+++ Utilidades/Util.Impresion.Web/Entities/GuiaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Util.Impresion.Web.Entities
+{
+    public class GuiaDbContext:DbContext
+    {
+        public GuiaDbContext(DbContextOptions opciones):base(opciones) {
+
+        }
+    }
+}
