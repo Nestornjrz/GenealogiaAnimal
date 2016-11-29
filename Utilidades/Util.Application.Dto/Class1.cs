d1bdc1cad4e620c5c1dbf52168444fd455fff5ab
Nestornjrz <nestornjrz@hotmail.com>
2016-11-29 14:58:51 -0300

    Se agrega el proyecto del dto

---
+++ Utilidades/Util.Application.Dto/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Util.Application.Dto
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
---
+++ Utilidades/Util.Application.Dto/ProveeClienteDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Util.Application.Dto
+{
+    public class ProveeClienteDto
+    {
+        public int ProveeClienteId { get; set; }
+        public string Nombre { get; set; }
+        public bool ProveedorSn { get; set; }
+        public bool ClienteSn { get; set; }
+
+    }
+}
