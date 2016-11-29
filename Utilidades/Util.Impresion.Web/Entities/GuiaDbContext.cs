387124abd442ce3231e437607fcc73d23af0b84d
Nestornjrz <nestornjrz@hotmail.com>
2016-11-29 15:01:34 -0300

    Se configura para que apunte a una base de datos local de SQLEXPRESS

--- Utilidades/Util.Impresion.Web/Entities/GuiaDbContext.cs
+++ Utilidades/Util.Impresion.Web/Entities/GuiaDbContext.cs
@@ -13,7 +13,14 @@ namespace Util.Impresion.Web.Entities {
         public virtual DbSet<ProveeClientes> ProveeClientes { get; set; }
 
         public GuiaDbContext(DbContextOptions opciones) : base(opciones) {
+            
+        }
+        public GuiaDbContext() {
 
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Guia;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
